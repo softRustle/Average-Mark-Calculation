cfcbfd28a8ed67ad939503ae6b38cc0c055b3187
justAnra <38633330+justAnra@users.noreply.github.com>
2023-06-15 17:25:36 +0300

    Basic refactoring
    - split into functions
    - file processing (reading)
    - example files added

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
+import os
+
 marks_dict = {'удовлетворительно': 3, 'хорошо': 4, 'отлично': 5}
-example_marks_list = ['зачтено', 'удовлетворительно', 'хорошо', 'хорошо', 'удовлетворительно', 'отлично', 'зачтено',
-                      'удовлетворительно', 'удовлетворительно']
 
-marks_counter = 0
-marks_sum = 0
 
-for mark in example_marks_list:
-    if mark in marks_dict.keys():
-        marks_sum += marks_dict[mark]
-        marks_counter += 1
+def get_file_names(basic_path):
+    names_list = []
+    for root, dirs, files in os.walk(basic_path):
+        for file in files:
+            names_list.append(os.path.join(basic_path, file))
+    return names_list
 
-print(f'Средний балл диплома {marks_sum/marks_counter:.2f}')
+
+def get_path():
+    path = '/\\Example files'
+    return path
+
+
+def read_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        marks_list = f.read().split()
+    return marks_list
+
+
+def write_file():
+    pass
+
+
+def calculate_avg_mark(marks_list):
+    marks_counter = 0
+    marks_sum = 0
+
+    for mark in marks_list:
+        if mark in marks_dict.keys():
+            marks_sum += marks_dict[mark]
+            marks_counter += 1
+    avg_mark = marks_sum / marks_counter
+    return f'{avg_mark:.2f}'
+
+
+current_path = get_path()
+names = get_file_names(current_path)
+for name in names:
+    print(calculate_avg_mark(read_file(name)))
